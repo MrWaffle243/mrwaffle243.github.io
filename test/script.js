514252ced9286506eca0ef452b38daf5b90d9fe2
mrwaffle243 <conorlyons36@gmail.com>
2025-10-12 00:36:22 +0100

    Test for other website

---
+++ test/script.js
@@ -0,0 +1,36 @@
+// Elements
+const searchInput = document.getElementById("search-input");
+const searchBtn = document.getElementById("search-btn");
+const result = document.getElementById("result")
+
+// Vars
+let songs = []
+let deezerURL = 'https://api.deezer.com/search?'
+// https://api.deezer.com/search?q=track:"i need a dollar"
+
+// Functions
+const songSearch = async () => {
+    const searchTerm = searchInput.value;
+    if (!searchTerm) {
+        alert("Please enter a song name");
+        return;
+    }
+
+    const searchQuery = `q=track:"${searchTerm}"`
+    console.log(deezerURL + searchQuery);
+
+    try {
+        const res = await fetch(deezerURL + searchQuery);
+        const data = await res.json();
+        console.log(data);
+    } catch (err) {
+        console.log(err);
+        alert("Song not found");
+        return;
+    }
+};
+
+// Event Listeners
+searchBtn.addEventListener("click", 
+    songSearch
+);
